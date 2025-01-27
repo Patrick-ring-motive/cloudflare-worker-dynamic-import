27f080fd9f879a048286e218e6c78a528d180f78
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-01-27 10:13:21 -0600

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,17 @@
+import runner from './runner.js';
+
+async function init(){
+  if(init.result)return init.result;
+   if(!init.running)init.running = runner.importModule(`https://raw.githubusercontent.com/Patrick-ring-motive/cloudflare-worker-dynamic-import/refs/heads/main/example.js?${new Date().getTime()}`);
+   await init.running;
+   init.result = runner.exports.default;
+   return init.result;
+}
+
+
+export default {
+  async fetch(request, env, ctx) {
+    const onReq = init.result ?? (await init());
+    return onReq(...arguments);
+  },
+};
