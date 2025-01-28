b4853918af2676b3d74ed4cd13dec7b24656d8ff
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-01-28 08:07:28 -0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,12 +2,14 @@ import runner from './runner.js';
 
 async function init(){
   if(init.result)return init.result;
-   if(!init.running)init.running = runner.importModule(`https://raw.githubusercontent.com/Patrick-ring-motive/kaleb/refs/heads/main/worker.js?${new Date().getTime()}`);
+   if(!init.running)init.running = runner.importModules(
+    'https://cdn.jsdelivr.net/npm/core-js/+esm',
+    `https://raw.githubusercontent.com/Patrick-ring-motive/kaleb/refs/heads/main/worker.js?${new Date().getTime()}`
+    );
    await init.running;
    init.result = runner.exports.default;
    return init.result;
 }
-
 
 export default {
   async fetch(request, env, ctx) {
