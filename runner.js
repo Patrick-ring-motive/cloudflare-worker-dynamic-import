d205c72bbdb8ebfe2e52b740e68aedc2033eea4c
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-02-24 10:38:38 -0600

    Update runner.js

--- runner.js
+++ runner.js
@@ -13,49 +13,42 @@ const interpreters = {
     sandBox: false,
   }),
 };
-const CacheMap = (()=>{
-  const objDefProp = (obj,prop,value) =>Object.defineProperty(obj,prop,{
-    value:value,
-    enumerable:false,
-    writable:true,
-    configurable:true
-  });
-  const isSymbol = x => typeof x === 'symbol' || x instanceof Symbol;
-  const mapSet = (map,key,value)=>Map.prototype.set.call(map,key,value);
-  const mapGet = (map,key,value)=>Map.prototype.get.call(map,key);
-  return class CacheMap extends Map{
-    constructor(iter){
-      super();
-      if(!this['&weakMap']){
-        objDefProp(this,'&weakMap',new WeakMap());
+
+const WeakRefMap = (()=>{
+  const $weakRefMap = Symbol('*weakRefMap');
+  return class WeakRefMap extends Map {
+      constructor() {
+        super();
+        this[$weakRefMap] = new Map();
       }
-      const init  = new Map(iter);
-      for (const [key, value] of init) {
-        this.set(key, value);
+
+      get(key) {
+        const ref = this[$weakRefMap].get(key);
+        const value = ref?.deref?.();
+        if (value === undefined) {
+          this[$weakRefMap].delete(key);
+        }
+        return value;
+      }
+
+      set(key, value) {
+        this[$weakRefMap].set(key, new WeakRef(value));
+        return this;
+      }
+
+      delete(key) {
+        return this[$weakRefMap].delete(key);
+      }
+
+      has(key) {
+        const value = this[$weakRefMap].get(key)?.deref?.();
+        if (value === undefined) {
+          this[$weakRefMap].delete(key);
+          return false;
+        }
+        return true;
       }
     }
-    get(key){
-      return this['&weakMap'].get(mapGet(this,key));
-    }
-    set(key,value){
-      let weakMapKey = mapGet(this,key);
-      if(!isSymbol(weakMapKey))weakMapKey = Symbol(key);
-      this['&weakMap'].set(weakMapKey,value);
-      return mapSet(this,key,weakMapKey);
-    }
-    has(key){
-      return this['&weakMap'].get(mapGet(this,key)) !== undefined;
-    }
-    delete(key) {
-    const weakMapKey = super.get(key);
-    const hasKey = super.has(key);
-    if (hasKey) {
-      this["&weakMap"].delete(weakMapKey);
-      super.delete(key);
-    }
-    return hasKey;
-    }
-  }
   })();
 
 const isArray = x => Array.isArray(x) || x instanceof Array;
@@ -74,8 +67,8 @@ globalThis.fetch = function fetch(){
 };
 
 const cache = {
-  module : new CacheMap(),
-  script : new CacheMap()
+  module : new WeakRefMap(),
+  script : new WeakRefMap()
 }; 
 
 const fetchText = async function fetchText(){
