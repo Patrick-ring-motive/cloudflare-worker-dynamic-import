5809b7955546c428a817facc26e53c386e9c2861
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-09-13 17:17:16 -0500

    Update runner.js

--- runner.js
+++ runner.js
@@ -6,17 +6,31 @@ const interpreter = new Sval({
     sandBox: false,
 });
 
-const $fetch = globalThis.fetch;
-globalThis.fetch = Object.setPrototypeOf(function fetch(...args){
-  try{
-    return await $fetch(...args);
-  }catch(e){
-    return new Response(Object.getOwnPropertyNames(e??{}).map(x=>`${x} : ${e[x]}`).join(''),{
-      status : 569,
-      statusText:e?.message
-    });
-  }
-},$fetch); 
+(()=>{
+    const $fetch = globalThis.fetch;
+    globalThis.fetch = Object.setPrototypeOf(function fetch(...args){
+      try{
+        return await $fetch(...args);
+      }catch(e){
+        return new Response(Object.getOwnPropertyNames(e??{}).map(x=>`${x} : ${e[x]}`).join(''),{
+          status : 569,
+          statusText:e?.message
+        });
+      }
+    },$fetch);
+})();
+
+(()=>{
+    const $Request = globalThis.Request;
+    globalThis.Request = class Request extends $Request{
+        constructor(...args){
+            if(/^(101|204|205|304)$/.test(args?.[1]?.status)){
+                (args??[])[0] = null;
+            }
+            return super(...args);
+        }
+    }
+})();
 
 const fetchText = async function fetchText(){
   const res = await fetch(...arguments);
