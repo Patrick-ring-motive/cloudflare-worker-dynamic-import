fd8a894a571758287573f7413a0b40640ff5b74d
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-09-13 17:19:11 -0500

    Update runner.js

--- runner.js
+++ runner.js
@@ -1,44 +1,26 @@
-import Sval from './sval.js';
+import { importModule } from './runner.js';
 
-const interpreter = new Sval({
-    ecmaVer: 'latest',
-    sourceType: 'module',
-    sandBox: false,
-});
-
-(()=>{
-    const $fetch = globalThis.fetch;
-    globalThis.fetch = Object.setPrototypeOf(function fetch(...args){
-      try{
-        return await $fetch(...args);
-      }catch(e){
-        return new Response(Object.getOwnPropertyNames(e??{}).map(x=>`${x} : ${e[x]}`).join(''),{
-          status : 569,
-          statusText:e?.message
-        });
+const workerURL = `https://raw.githubusercontent.com/Patrick-ring-motive/venusaur/refs/heads/main/venusaur.js?${new Date().getTime()}`;
+const isPromise = x => x instanceof Promise || typeof x?.then === 'function' || x?.constructor?.name === 'Promise';
+let init;
+export default {
+  async fetch(request, env, ctx) {
+    try{
+      if(!init){
+        init = importModule(workerURL);
       }
-    },$fetch);
-})();
-
-(()=>{
-    const $Request = globalThis.Request;
-    globalThis.Request = class Request extends $Request{
-        constructor(...args){
-            if(/^(101|204|205|304)$/.test(args?.[1]?.status)){
-                (args??[])[0] = null;
-            }
-            return super(...args);
-        }
+      if(isPromise(init)){
+        init = await init;
+      }
+      console.log(init);
+      const { onRequest } = init;
+      return await onRequest(...arguments);
+    }catch(e){
+      console.warn(e,...arguments);
+      return new Response(Object.getOwnPropertyNames(e??{}).map(x=>`${x} : ${e[x]}`).join(''),{
+        status : 569,
+        statusText:e?.message
+      });
     }
-})();
-
-const fetchText = async function fetchText(){
-  const res = await fetch(...arguments);
-  return await res.text();
-};
-
-export const importModule = async function importModule(url){
-  const mod = await fetchText(url);
-  interpreter.run(mod);
-  return interpreter['exports'];
+  },
 };
