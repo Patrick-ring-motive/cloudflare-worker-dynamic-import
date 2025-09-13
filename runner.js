f38810fc2e492c5c35008194dce6cf217838a162
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-09-12 20:36:04 -0500

    Update runner.js

--- runner.js
+++ runner.js
@@ -1,27 +1,10 @@
-import './core.js';
-import Sval from './sval.js'
+import Sval from './sval.js';
 
-globalThis.WeakRef ??= (()=>function WeakRef(ref){
-	const $this = new.target ? this : Object.create(WeakRef.prototype);
-	$this.deref = () => ref;
-	return $this;
-})();
-
-const interpreters = {
-  module : new Sval({
+const interpreter = new Sval({
     ecmaVer: 'latest',
     sourceType: 'module',
     sandBox: false,
-  }),
-  script : new Sval({
-    ecmaVer: 'latest',
-    sourceType: 'script',
-    sandBox: false,
-  }),
-};
-
-
-const isArray = x => Array.isArray(x) || x instanceof Array || x?.constructor?.name === 'Array';
+});
 
 const $fetch = globalThis.fetch;
 globalThis.fetch = Object.setPrototypeOf(function fetch(...args){
@@ -33,68 +16,15 @@ globalThis.fetch = Object.setPrototypeOf(function fetch(...args){
       statusText:e?.message
     });
   }
-},$fetch);
-
-const cache = {
-  module : new Map(),
-  script : new Map()
-}; 
+},$fetch); 
 
 const fetchText = async function fetchText(){
   const res = await fetch(...arguments);
   return await res.text();
 };
 
-const runner = interpreters['module'];
-
-runner.compileModule = async function compileModule(urlObj,options,type="module"){
-  const cacheMap = cache[type];
-  const url = String(urlObj?.url ?? urlObj);
-  const key = url.split(/[?#]/).shift();
-  let mod;
-  if(cacheMap.has(key)){
-    mod = cacheMap.get(key);
-  }else{
-    mod = fetchText(...arguments);
-    cacheMap.set(key,mod);
-  }
-  if(mod instanceof Promise){
-    mod = await mod;
-    mod = interpreters[type].parse(mod);
-    cacheMap.set(key,mod);
-  }
-  return cacheMap.get(key);
+export const importModule = async function importModule(url){
+  const mod = await fetchText(url);
+  interpreter.run(mod);
+  return interpreter['export'];
 };
-
-runner.importModule = async function importModule(urlObj,options,type="module"){
-  const mod = await runner.compileModule(arguments);
-  interpreters[type].run(mod);
-  return interpreters[type];
-};
-
-
-
-runner.compileScript = async function compileScript(urlObj,options){
-  return runner.compileModule(urlObj,options,"script");
-};
-
-runner.importScript = async function importScript(urlObj,options){
-  return runner.importModule(urlObj,options,"script");
-};
-
-runner.compileCode = async function compileCode(urlObj,options){
-  try{
-    return await runner.compileModule(urlObj,options,"script");
-  }catch{
-    return await runner.compileModule(urlObj,options);
-  }
-}
-
-runner.importModules = async function importModules(...args){
-  args = args.map(x=>isArray(x)?runner.compileCode(...x):runner.compileCode(x));
-  args = await Promise.all(args);
-  args.map(x=>runner.run(x));
-  return runner;
-};
-
-export default runner;
