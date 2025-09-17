965175aa7508c6bec79d3fca539cf51e80aac661
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-09-17 12:09:44 -0500

    Update runner.js

--- runner.js
+++ runner.js
@@ -1,26 +1,50 @@
-import { importModule } from './runner.js';
+import Sval from './sval.js';
 
-const workerURL = `https://raw.githubusercontent.com/Patrick-ring-motive/venusaur/refs/heads/main/venusaur.js?${new Date().getTime()}`;
-const isPromise = x => x instanceof Promise || typeof x?.then === 'function' || x?.constructor?.name === 'Promise';
-let init;
-export default {
-  async fetch(request, env, ctx) {
-    try{
-      if(!init){
-        init = importModule(workerURL);
+const interpreter = new Sval({
+    ecmaVer: 'latest',
+    sourceType: 'module',
+    sandBox: false,
+});
+
+(()=>{
+    const $fetch = globalThis.fetch;
+    globalThis.fetch = Object.setPrototypeOf(async function fetch(...args){
+      try{
+        return await $fetch(...args);
+      }catch(e){
+        console.warn(e,...arguments);
+        return new Response(Object.getOwnPropertyNames(e??{}).map(x=>`${x} : ${e[x]}`).join(''),{
+          status : 569,
+          statusText:e?.message
+        });
       }
-      if(isPromise(init)){
-        init = await init;
-      }
-      console.log(init);
-      const { onRequest } = init;
-      return await onRequest(...arguments);
-    }catch(e){
-      console.warn(e,...arguments);
-      return new Response(Object.getOwnPropertyNames(e??{}).map(x=>`${x} : ${e[x]}`).join(''),{
-        status : 569,
-        statusText:e?.message
-      });
+    },$fetch);
+})();
+
+(()=>{
+    const $Response = globalThis.Response;
+    globalThis.Response = class Response extends $Response{
+        constructor(...args){
+            try{
+                if(/^(101|204|205|304)$/.test(args?.[1]?.status)){
+                    (args??[])[0] = null;
+                    delete (args??[])[1].body;
+                }
+                return super(...args);
+            }catch(e){
+                console.warn(e,...args);
+            }
+        }
     }
-  },
+})();
+
+const fetchText = async function fetchText(){
+  const res = await fetch(...arguments);
+  return await res.text();
+};
+
+export const importModule = async function importModule(url){
+  const mod = await fetchText(url);
+  interpreter.run(mod);
+  return interpreter['exports'];
 };
