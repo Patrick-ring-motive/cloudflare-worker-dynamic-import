6ae2656db32cefc7117787ee96da3a5bf15a68eb
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-23 07:45:59 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,26 +1,33 @@
-import {
-  env
-} from "cloudflare:workers"
+import { env } from "cloudflare:workers"
 import {
   importModule
 } from './runner.js';
 
-globalThis.env = env;
+globalThis.env=env;
 
-const workerURL = `${env.WORKER_URL}?${new Date().getTime()}`;
+const debug = (...args)=>{
+  if(env.mode === 'DEBUG'){
+    console.log(...args);
+  }
+};
+
+let workerURL = env.WORKER_URL;
 const isPromise = x => x instanceof Promise || typeof x?.then === 'function' || x?.constructor?.name === 'Promise';
 let init;
 export default {
   async fetch(request, env, ctx) {
     try {
-      if (env.mode === 'DEV' || !init) {
-        if (!isPromise(init))
+      debug(workerURL);
+      if (['DEV','DEBUG'].includes(env.mode)||!init) {
+        if(!isPromise(init)){
+          workerURL = `${env.WORKER_URL}?${crypto.randomUUID()+new Date().getTime()+Math.random()}`;
           init = importModule(workerURL);
+        }
       }
       if (isPromise(init)) {
         init = await init;
       }
-      console.log(init);
+      debug(init);
       const {
         onRequest
       } = init;
